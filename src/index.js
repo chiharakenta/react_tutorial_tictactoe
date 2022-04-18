3683fd378609218f1a58a4ab86fe05fe62be73a6
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2022-04-18 16:25:39 +0900

    fix: typo

--- src/index.js
+++ src/index.js
@@ -53,13 +53,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      setpNumber: 0,
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.setpNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]) {
@@ -70,21 +70,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
-      setpNumber: history.length,
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
 
   jumpTo(step) {
     this.setState({
-      setpNumber: step,
+      stepNumber: step,
       xIsNext: (step % 2) === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[this.state.setpNumber];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
